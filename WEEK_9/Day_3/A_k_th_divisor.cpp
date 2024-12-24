c821babdc177250f6cc2613c50bd0562299efd57
RonyInfiniteLoop <164559990+RonyInfiniteLoop@users.noreply.github.com>
2024-12-24 21:50:59 +0600

    Add files via upload

---
+++ WEEK_9/Day_3/A_k_th_divisor.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    ll n, k;
+    cin>>n>>k;
+    vector<ll>divisor;
+    for(ll i=1; i*i<=n; i++)
+    {
+        if(n%i == 0)
+        {
+            divisor.push_back(i);
+            if((n/i) != i)
+            {
+                divisor.push_back(n/i);
+            }
+        }
+    }
+    sort(divisor.begin(), divisor.end());
+    if(divisor.size() < k)
+    {
+        cout<<-1<<endl;
+    }
+    else{
+    
+        cout<<divisor[k-1]<<endl;
+    }
+    
+    return 0;
+}
